4abf4f45897ba9b6231bec826425e4d0885ec8a0
katie-060330 <kate.lonsway@outlook.com>
2024-10-25 19:48:10 -0400

    adding select meue for leauges on tonight

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let apiURL = "https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=d1954e19d0c4e0d00515d9e4290548df"
 const sportList = document.querySelector(".Sports_tonight")
+const nonDoup = document.getElementById("nonDoup") 
 
-async function test(){
+async function retriveData(){
     
 const response = await fetch(apiURL); 
     if(!response.ok){
@@ -10,27 +11,34 @@ const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
 
+    //?storing the non doups in a set 
+    let list = new Set();
 
-    let list; 
-
+    //? adding the leages that are on tonigth into a list
     for(let i = 0; i < data.length; i++){
-        console.log(data[i].sport_key);
-        const sportingEvent = document.createElement("li"); 
-        sportingEvent.textContent = data[i].sport_key; 
-        sportList.append(sportingEvent);
+        if(!(list.has(data[i].sport_key))){
+            list.add(data[i].sport_key)
+        }
 
     }
-
-    // function renderFavorites() {
-    //     favCitiesList.innerHTML = "";
-    //     favCities.forEach(city => {
-    //       const cityItem = document.createElement("li");
-    //       cityItem.textContent = city;
-    //       favCitiesList.appendChild(cityItem);
-    //     });
-    //   }
+    populateSelectMenu(list);
 
 
 }
 
-test().catch(error => console.error(error));
+const populateSelectMenu = (list) =>{
+ 
+    list.forEach( elem =>{
+        console.log(elem);
+        const league = document.createElement('option')
+        league.value = elem; 
+        league.text = elem; 
+        nonDoup.append(league)
+    }
+
+    );
+
+}
+
+retriveData().catch(error => console.error(error));
+
