2bca367ae91898e07335d797d0c36e0cef3daa93
katie-060330 <kate.lonsway@outlook.com>
2024-10-25 20:09:03 -0400

    adding on click

--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 let apiURL = "https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=d1954e19d0c4e0d00515d9e4290548df"
 const sportList = document.querySelector(".Sports_tonight")
 const nonDoup = document.getElementById("nonDoup") 
+const leage = document.getElementById("leaugeName")
+const stats = document.getElementById("stats")
 
 async function retriveData(){
     
@@ -22,12 +24,30 @@ const response = await fetch(apiURL);
 
     }
     populateSelectMenu(list);
+    
+//TODO when the user selects the league it takes it to the leauge page 
+nonDoup.onchange = function (){
+    const selectedOption = nonDoup.value; 
+    setLeague(selectedOption);   
 
+}
+const setLeague = (selectedOption) =>{
+    let games = new Set();
+    for(let i = 0; i < data.length; i++){
+        if((selectedOption == (data[i].sport_key))){
+            games.add(data[i])
+        }
+
+    }
+    console.log(games);
+
+}
 
 }
 
 const populateSelectMenu = (list) =>{
  
+    //?adding select options from the api for what is on tonight
     list.forEach( elem =>{
         console.log(elem);
         const league = document.createElement('option')
@@ -39,6 +59,7 @@ const populateSelectMenu = (list) =>{
     );
 
 }
+
 
 retriveData().catch(error => console.error(error));
 
